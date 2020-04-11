4c0d7fd9870865239ba478b1ba4b7ad23060f5ed
avudaiappan-cse <avudaiappan.cse@gmail.com>
2020-04-11 13:50:51 +0530

    Update DB

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 3000 || process.env.PORT;
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/blog",
+  process.env.MONGO_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
